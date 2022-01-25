5c2fb8325b8080a0cd9567a5bbaf06ee47c05559
unknown <anhboy6ss@gmail.com>
2022-01-25 15:48:03 +0700

    update

--- Search-B5-TongTienPhongKhachSan/index.php
+++ Search-B5-TongTienPhongKhachSan/index.php
@@ -1,23 +1,18 @@
 <?php
 $matrix = [
     [0, 1, 1, 2],
-    [0, 5, 0, 1],
-    [2, 0, 3, 3]];
+    [0, 5, 0, 0],
+    [2, 0, 3, 3]
+];
 
-function totalMoney($arr)
-{
-    $total = 0;
-
-    for ($i = 0; $i < count($arr); $i++) {
-        for ($j = 0; $j < count($arr[$i]); $j++) {
-            if ($arr[$j][$i] > 0) {
-                $total += $arr[$j][$i];
-            } else {
-                break;
-            }
+$total = 0;
+for ($i = 0; $i < count($matrix[0]); $i++) {
+    for ($j = 0; $j < count($matrix); $j++) {
+        if ($matrix[$j][$i] == 0) {
+            break;
         }
+    $total += $matrix[$j][$i];
     }
-    return $total;
 }
 
-echo totalMoney($matrix);
+echo $total;
